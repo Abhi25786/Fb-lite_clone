d22cfe5741a57a0f5842d618b87361b6b91fc4fc
admin <admin@admins-iMac-4.local>
2022-03-21 17:54:27 +0530

    af

--- screens/profilescreen/profilecode/Profile.js
+++ screens/profilescreen/profilecode/Profile.js
@@ -17,18 +17,12 @@ function Profile() {
                             <View style={{ marginBottom: 10, justifyContent: "center" }}>
 
 
-                                {/* <View style={{ height: 400, width: 400 }}>
-                                    <Image source={require('../profileimages/profile_user.jpg')} style={{ width: 400, height: 200, position: "absolute" }} />
-                                    <View style={profile.cameralogo}>
-                                        <Image source={require('../profileimages/photo-camera.png')} style={styles.home} />
-
-                                    </View>
-
-                                </View> */}
                                 <View style={profile.container}>
                                     <Image source={require('../profileimages/profile_user.jpg')} style={profile.profilebackimg} />
 
                                     <Image source={require('../profileimages/profile_user.jpg')} style={profile.profileimg} />
+                                    <Image source={require('../profileimages/photo-camera1.png')} style={{ position: "absolute", height: 35, width: 35, alignItems: "flex-end", right: 0, bottom: 120 }} />
+                                    <Image source={require('../profileimages/photo-camera1.png')} style={{ position: "absolute", height: 35, width: 35, alignItems: "flex-end", bottom: 50, right: 100, borderColor: "white", borderWidth: 3, borderRadius: 20 }} />
                                 </View>
 
 
@@ -43,11 +37,11 @@ function Profile() {
                             <View style={{ flexDirection: "row", justifyContent: "space-around" }}>
                                 <View style={profile.adddbtn}>
                                     <Image source={require('../profileimages/add.png')} style={profile.iconscss} />
-                                    <Text>Add to story</Text>
+                                    <Text style={{ color: "black" }}>Add to story</Text>
                                 </View>
                                 <View style={profile.editbtn}>
                                     <Image source={require('../profileimages/pen.png')} style={profile.iconscss} />
-                                    <Text>Edit profile</Text>
+                                    <Text style={{ color: "black" }}>Edit profile</Text>
                                 </View>
                                 <View style={{ justifyContent: "center", backgroundColor: "lightgray", alignItems: "center", borderRadius: 10, paddingLeft: 10 }}>
                                     <Image source={require('../profileimages/menu.png')} style={profile.iconscss} />
